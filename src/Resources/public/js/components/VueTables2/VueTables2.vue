9d71f755b4133d9e40bb5a0d7692acfc789738a3
evrinoma <evrinoma@gmail.com>
2021-03-09 01:30:42 +0300

    fix front logic

--- src/Resources/public/js/components/VueTables2/VueTables2.vue
+++ src/Resources/public/js/components/VueTables2/VueTables2.vue
@@ -1,124 +1,137 @@
 <template>
-    <div>
-        <filter-bar></filter-bar>
-        <vuetable ref="vuetable"
-                  :api-url="apiUrl"
-                  :fields="fields"
-                  :per-page="5"
-                  :multi-sort="true"
-                  multi-sort-key="ctrl"
-                  :sort-order="sortOrder"
-                  pagination-path=""
-                  @vuetable:pagination-data="onPaginationData"
-                  @vuetable:cell-clicked="onCellClicked"
-                  :append-params="moreParams"
-        ></vuetable>
-        <div class="vuetable-pagination ui basic segment grid">
-            <vuetable-pagination-info ref="paginationInfo">
-            </vuetable-pagination-info>
+  <div>
+    <filter-bar></filter-bar>
+    <vuetable ref="vuetable"
+              :http-fetch= "loadFetch"
+              :api-url="apiUrl"
+              :fields="fields"
+              :per-page="perPage"
+              :multi-sort="true"
+              multi-sort-key="ctrl"
+              :sort-order="sortOrder"
+              pagination-path=""
+              @vuetable:pagination-data="onPaginationData"
+              @vuetable:cell-clicked="onCellClicked"
+              :append-params="moreParams"
+    ></vuetable>
+    <div class="vuetable-pagination ui basic segment grid">
+      <vuetable-pagination-info ref="paginationInfo">
+      </vuetable-pagination-info>
 
-            <vuetable-pagination ref="pagination"
-                                 @vuetable-pagination:change-page="onChangePage"
-            ></vuetable-pagination>
-        </div>
+      <vuetable-pagination ref="pagination"
+                           @vuetable-pagination:change-page="onChangePage"
+      ></vuetable-pagination>
     </div>
+  </div>
 </template>
 
 <script>
-    import Vue from 'vue';
-    import VueEvents from 'vue-events';
-    import Vuetable from 'vuetable-2/src/components/Vuetable';
-    import VuetablePagination from 'vuetable-2/src/components/VuetablePagination';
-    import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo'
-    import CustomActions from './CustomActions';
-    import FilterBar from './FilterBar';
-    import axios from 'axios';
+import Vue from 'vue';
+import VueEvents from 'vue-events';
+import Vuetable from 'vuetable-2/src/components/Vuetable';
+import VuetablePagination from 'vuetable-2/src/components/VuetablePagination';
+import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo'
+import CustomActions from './CustomActions';
+import FilterBar from './FilterBar';
+import axios from 'axios';
 
-    Vue.use(VueEvents);
-    Vue.use(axios);
-    Vue.component('custom-actions', CustomActions);
-    Vue.component('filter-bar', FilterBar);
+Vue.use(VueEvents);
+Vue.use(axios);
+Vue.component('custom-actions', CustomActions);
+Vue.component('filter-bar', FilterBar);
 
-    let component = {
-        components: {
-            Vuetable,
-            VuetablePagination,
-            VuetablePaginationInfo
-        },
-        props: {
-            fields: {
-                type: Array,
-                required: true
-            },
-            apiUrl: {
-                type: String,
-                required: true
-            },
-            apiUrlDelete: {
-                type: String,
-                required: true
-            },
-            domainClass: {
-                type: String,
-                required: true
-            },
-            sortOrder: {
-                type: Array,
-                default() {
-                    return []
-                }
-            },
-        },
-        data() {
-            return {
-                moreParams: {
-                    class: this.domainClass,
-                },
-            }
-        },
-        mounted() {
-            this.$events.$on('filter-set', eventData => this.onFilterSet(eventData));
-            this.$events.$on('filter-reset', eventData => this.onFilterReset());
-            this.$events.$on('table-save', eventData => this.onUpdate());
-            this.$events.$on('custom-actions-delete', eventData => this.onDelete(eventData));
-        },
-        methods: {
-            onChangePage(page) {
-                this.$refs.vuetable.changePage(page)
-            },
-            onPaginationData(paginationData) {
-                this.$refs.pagination.setPaginationData(paginationData);
-                this.$refs.paginationInfo.setPaginationData(paginationData);
-            },
-            onCellClicked(data, field, event) {
-                this.$events.fire('info-set', data);
-            },
-            onFilterSet(filterText) {
-                this.moreParams.filter = filterText;
-                Vue.nextTick(() => this.$refs.vuetable.refresh());
-            },
-            onFilterReset() {
-                delete this.moreParams.filter;
-                Vue.nextTick(() => this.$refs.vuetable.refresh());
-            },
-            onUpdate() {
-                Vue.nextTick(() => this.$refs.vuetable.refresh());
-            },
-            onDelete(data) {
-                axios
-                    .delete(this.apiUrlDelete, data)
-                    .then(response => (this._axiosResponse('custom-actions-delete', response)));
-            },
-            _axiosResponse(type, response) {
-                switch (type) {
-                    case 'custom-actions-delete':
-                        Vue.nextTick(() => this.$refs.vuetable.refresh());
-                        break;
-                }
-            }
-        }
-    };
+let component = {
+  components: {
+    Vuetable,
+    VuetablePagination,
+    VuetablePaginationInfo
+  },
+  props: {
+    fields: {
+      type: Array,
+      required: true
+    },
+    apiUrl: {
+      type: String,
+      required: true
+    },
+    apiUrlDelete: {
+      type: String,
+      required: true
+    },
+    domainClass: {
+      type: String,
+      required: true
+    },
+    sortOrder: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    perPage: {
+      type: Number,
+      default() {
+        return 5
+      }
+    },
+  },
+  data() {
+    return {
+      moreParams: {
+      },
+    }
+  },
+  mounted() {
+    this.doLoad();
+  },
+  methods: {
+    onChangePage(page) {
+      this.$refs.vuetable.changePage(page)
+    },
+    onPaginationData(paginationData) {
+      this.$refs.pagination.setPaginationData(paginationData);
+      this.$refs.paginationInfo.setPaginationData(paginationData);
+    },
+    onCellClicked(data, field, event) {
+      this.$events.fire('info-set', data);
+    },
+    onFilterSet(filterText) {
+      this.moreParams.filter = filterText;
+      Vue.nextTick(() => this.$refs.vuetable.refresh());
+    },
+    onFilterReset() {
+      delete this.moreParams.filter;
+      Vue.nextTick(() => this.$refs.vuetable.refresh());
+    },
+    onUpdate() {
+      Vue.nextTick(() => this.$refs.vuetable.refresh());
+    },
+    onDelete(data) {
+      axios
+          .delete(this.apiUrlDelete, data)
+          .then(response => (this._axiosResponse('custom-actions-delete', response)));
+    },
+    doLoad() {
+      this.$events.$on('filter-set', eventData => this.onFilterSet(eventData));
+      this.$events.$on('filter-reset', eventData => this.onFilterReset());
+      this.$events.$on('table-save', eventData => this.onUpdate());
+      this.$events.$on('custom-actions-delete', eventData => this.onDelete(eventData));
+    },
+    loadFetch(apiUrl, httpOptions) {
+      httpOptions.params['class'] = this.domainClass;
+      return axios.get(apiUrl, httpOptions);
+    },
+    _axiosResponse(type, response) {
+      switch (type) {
+        case 'custom-actions-delete':
+          Vue.nextTick(() => this.$refs.vuetable.refresh());
+          break;
+      }
+    }
+  }
+};
 
 
-    export default component
+export default component
 </script>
